e7f53e375bdf668a40f4576b68389b69ffbbc681
siva1252 <sivaduddukuri1252@gmail.com>
2026-07-29 09:40:00 +0530

    Bootstrap Django project settings and entrypoints

---
+++ main_analytic/urls.py
@@ -0,0 +1,42 @@
+"""
+URL configuration for main_analytic project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse, FileResponse
+from django.conf import settings
+from django.conf.urls.static import static
+from django.shortcuts import render
+import os
+
+def home(request):
+    return render(request, 'create_site.html')   #this is the home page of the website
+
+def serve_tracker(request):
+    tracker_path = os.path.join(settings.BASE_DIR, 'static', 'tracker.js')
+    if os.path.exists(tracker_path):
+        return FileResponse(open(tracker_path, 'rb'), content_type='application/javascript')
+    return HttpResponse("// tracker.js not found", status=404)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home),
+    path('dashboard/', lambda r: render(r, 'dashboard.html')),
+    path('visitors-detail/', lambda r: render(r, 'visitors_detail.html')),
+    path('api/', include('analytic_core.urls')),
+    path('tracker.js', serve_tracker, name='serve-tracker'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT if hasattr(settings, 'STATIC_ROOT') else os.path.join(settings.BASE_DIR, 'static'))
